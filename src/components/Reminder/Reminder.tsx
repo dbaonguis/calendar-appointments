484e22e8e66371171eaec7c5b39c27ed68919cba
Donato Baonguis <dbaonguis@yahoo.com>
2021-03-21 13:43:56 -0400

    fix background and text color for reminder stub

--- src/components/Reminder/Reminder.tsx
+++ src/components/Reminder/Reminder.tsx
@@ -1,62 +1,13 @@
 import React from 'react';
 import { Card, CardActionArea } from '@material-ui/core';
 import Grid, { GridSize } from '@material-ui/core/Grid';
-import { v4 as uuidv4 } from 'uuid';
-import { format as dateFormat } from 'date-fns';
 import deepPurple from '@material-ui/core/colors/deepPurple';
-import { withStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { WithStyles, withStyles, Theme, createStyles } from '@material-ui/core/styles';
 import { Color } from './../../utils/colors';
 
 const styles = (theme: Theme) => createStyles({
-	dayCell: {
-		display: 'flex',
-		flex: '1 0 13%',
-		flexDirection: 'column',
-		border: '1px solid lightgray',
-		cursor: 'pointer'
-	},
-	dayCellOutsideMonth: {
-		display: 'flex',
-		flex: '1 0 13%',
-		flexDirection: 'column',
-		border: '1px solid lightgray',
-		backgroundColor: 'rgba( 211, 211, 211, 0.4 )',
-		cursor: 'pointer'
-	},
-	dateNumber: {
-		margin: 5,
-		height: '28px',
-		width: '28px',
-		fontSize: '0.85rem',
-		color: '#000',
-		backgroundColor: 'transparent'
-	},
-	todayAvatar: {
-		margin: 5,
-		height: '28px',
-		width: '28px',
-		fontSize: '0.85rem',
-		color: '#fff',
-		backgroundColor: deepPurple[400],
-	},
-	focusedAvatar: {
-		margin: 5,
-		height: '28px',
-		width: '28px',
-		fontSize: '0.85rem',
-		color: '#000',
-		backgroundColor: '#f1f1f1',
-	},
-	focusedTodayAvatar: {
-		margin: 5,
-		height: '28px',
-		width: '28px',
-		fontSize: '0.85rem',
-		color: '#fff',
-		backgroundColor: deepPurple[800],
-	},
-	remindersContainer: {
-		height: '100%'
+	card: {
+		backgroundColor: 'red',
 	}
 });
 
@@ -69,13 +20,13 @@ export interface ReminderProp {
   color: Color;
 }
 
-interface Props {
+interface Props extends WithStyles<typeof styles>{
   reminderObj: any;
 }
 
 const Reminder = (props: Props) => {
 
-  const { reminderObj } = props;
+  const { classes, reminderObj } = props;
 
   const rawTime = Object.keys(reminderObj)[0];
   let formattedTime = rawTime.split('_')[1];
@@ -96,20 +47,20 @@ const Reminder = (props: Props) => {
             
               //alert('hey!');
             }}>
-              3+ reminders, click for details.
+              {`[${formattedTime}] ${remindersPerTime.length} reminders here, click for details.`}
             </CardActionArea>
           </Card>
         </Grid>
       )}
       {remindersPerTime.length <= 3 && remindersPerTime.map((reminder: ReminderProp) => (
         <Grid item xs={colSpan}>
-          <Card>
+          <Card className={classes.card} style={{backgroundColor: reminder.color.background, color: reminder.color.text}}>
             <CardActionArea onClick={(evt) => {
               evt.stopPropagation();
             
               // alert('ey!');
             }}>
-              {reminder.message}
+              {`[${formattedTime}] ${reminder.message}`}
             </CardActionArea>
           </Card>
         </Grid>
